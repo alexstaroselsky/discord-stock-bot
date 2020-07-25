4839138bfbede70b2f38b4eb066f51d8418b5f49
Alexander Staroselsky <34102969+alexstaroselsky@users.noreply.github.com>
2020-07-24 19:25:39 -0600

    added comments

--- clients/api.js
+++ clients/api.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const instance = axios.create({
-  baseURL: 'https://sandbox.iexapis.com/stable/',
+  baseURL: 'https://sandbox.iexapis.com/stable/', // this is NOT the production endpoint, need to update
 });
 
 module.exports = instance;
